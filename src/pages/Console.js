05897e7c69bfcef670dc1bba159ac1ddb5f184db
aslamm <aslamm.anas29@gmail.com>
2020-05-10 15:16:03 -0400

    Finished building and styling of the application

--- src/pages/Console.js
+++ src/pages/Console.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Console = () => {
+  const [angleDown, setAngleDown] = useState(true);
+
   return (
     <section id="console">
       <header>
@@ -23,31 +25,119 @@ const Console = () => {
       </header>
       <section className="content-section">
         <section className="select-menu">
-            <ul className="list-container">
+          <ul className="list-container">
             <i class="fas fa-tachometer-alt"></i>
-                <a href="/">Dashboard</a>
-            </ul>
-            <ul className="list-container">
+            <a href="/">Dashboard</a>
+          </ul>
+          <ul className="list-container">
             <i class="fas fa-share-alt"></i>
             <a href="/console">Console</a>
-            </ul>
-            <ul className="list-container">
+          </ul>
+          <ul className="list-container">
             <i class="fas fa-list"></i>
             <a href="/tasks">Tasks</a>
-            </ul>
-            <ul className="list-container">
+          </ul>
+          <ul className="list-container">
             <i class="far fa-file"></i>
             <span>Templates</span>
-            <i class="fas fa-angle-down"></i>
-            </ul>
+            <i
+              className={`fas fa-angle-${!angleDown ? "up" : "down"}`}
+              onClick={() => setAngleDown(!angleDown)}
+            ></i>
+          </ul>
+          {!angleDown ? null : (
             <ul className="sub-list">
-            <span className="selected">Forms</span>
+              <span className="selected">Forms</span>
             </ul>
+          )}
         </section>
         <section className="content-area">
-            Content Comes Here!!!
+          <div className="box">
+            <div className="title-area">
+              <div className="title">Forms</div>
+            </div>
+            <div className="form-container">
+              <div className="sub-form">
+                <div className="sub-title-area">
+                  <div className="sub-title">Contracts</div>
+                  <i class="fas fa-plus"></i>
+                </div>
+                <div className="sub-contents">
+                  <div className="content">
+                    <p>Sample Basic</p>
+                    <p>Contract</p>
+                  </div>
+                  <div className="content">
+                    <p>Sample</p>
+                    <p>Contact Info</p>
+                    <p>Contract</p>
+                  </div>
+                  <div className="content">
+                    <p>Sample Initials</p>
+                    <p>The Terms</p>
+                    <p>Contract</p>
+                  </div>
+                </div>
+              </div>
+              <div className="sub-form">
+                <div className="sub-title-area">
+                  <div className="sub-title">Sub-Agreements</div>
+                  <i class="fas fa-plus"></i>
+                </div>
+                <div className="sub-contents">
+                  <div className="content">
+                    <p>Sample</p>
+                    <p>Design Proof</p>
+                  </div>
+                  <div className="content">
+                    <p>Sample</p>
+                    <p>Release Water</p>
+                  </div>
+                </div>
+              </div>
+              <div className="sub-form">
+                <div className="sub-title-area">
+                  <div className="sub-title">Questionnaires</div>
+                  <i class="fas fa-plus"></i>
+                </div>
+                <div className="sub-contents">
+                  <div className="content">
+                    <p>Sample Intake</p>
+                    <p>Questionnaire</p>
+                  </div>
+                  <div className="content">
+                    <p>Sample</p>
+                    <p>Resource/Info</p>
+                    <p>Sheet</p>
+                  </div>
+                </div>
+              </div>
+              <div className="sub-form">
+                <div className="sub-title-area">
+                  <div className="sub-title">Extras</div>
+                  <i class="fas fa-plus"></i>
+                </div>
+                <div className="sub-contents">
+                  <div className="content">
+                    <p>Extra Sample</p>
+                    <p>Contact</p>
+                  </div>
+                  <div className="content">
+                    <p>Sample</p>
+                    <p>Contact Info</p>
+                    <p>Contract</p>
+                  </div>
+                  <div className="content">
+                    <p>Sample Initials</p>
+                    <p>The Terms</p>
+                    <p>Contract</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </section>
-    </section>
+      </section>
     </section>
   );
 };
