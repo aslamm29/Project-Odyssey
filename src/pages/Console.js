3ca64ac6a34994aad2a1f200eeda9f967d645663
aslamm <aslamm.anas29@gmail.com>
2020-05-09 22:43:24 -0400

    created the console page

--- src/pages/Console.js
+++ src/pages/Console.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const Console = () => {
-return(
-    <div>
-        Console
-    </div>
-)
-}
+  return (
+    <section id="console">
+      <header>
+        <div className="logo">
+          <div className="logo-img" />
+        </div>
+        <div className="search-bar">
+          <i className="fas fa-search"></i>
+          <input
+            className="search-input"
+            placeholder="Search projects and clients"
+          />
+        </div>
+        <div className="menu-btn">
+          <i className="far fa-envelope"></i>
+          <i className="far fa-bell"></i>
+          <i className="fas fa-cog"></i>
+        </div>
+      </header>
+    </section>
+  );
+};
 
-export default Console
+export default Console;
