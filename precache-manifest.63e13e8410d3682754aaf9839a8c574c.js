9ed66e57af44d52c1174a73b5bcfc7406fde96b8
aslamm <aslamm.anas29@gmail.com>
2020-05-10 22:53:16 -0400

    Updates

---
+++ precache-manifest.63e13e8410d3682754aaf9839a8c574c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3079dba0e64f87f37b1fcb103baf9853",
+    "url": "/Project-Odyssey/index.html"
+  },
+  {
+    "revision": "cfb679ddce1ee6433e09",
+    "url": "/Project-Odyssey/static/css/main.88b0fd2c.chunk.css"
+  },
+  {
+    "revision": "352b65f82b2eb6dc41e0",
+    "url": "/Project-Odyssey/static/js/2.dc09aafe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Project-Odyssey/static/js/2.dc09aafe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfb679ddce1ee6433e09",
+    "url": "/Project-Odyssey/static/js/main.1eb56318.chunk.js"
+  },
+  {
+    "revision": "7d1b398d059f54e1c9eb",
+    "url": "/Project-Odyssey/static/js/runtime-main.b7917e9a.js"
+  },
+  {
+    "revision": "1990a2add6726636cc16876e2dc11066",
+    "url": "/Project-Odyssey/static/media/consoleLogo.1990a2ad.jpg"
+  }
+]);
