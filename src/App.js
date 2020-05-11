85a3b8473753423da609f50e8cc42f9be1c3648f
aslamm <aslamm.anas29@gmail.com>
2020-05-11 01:00:31 -0400

    testing urls

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from "react";
 import Dashboard from "./pages/Dashboard";
-//import Console from "./pages/Console";
-//import Tasks from "./pages/Tasks";
-//import { BrowserRouter, Route } from "react-router-dom";
+import Console from "./pages/Console";
+import Tasks from "./pages/Tasks";
+import { BrowserRouter, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Dashboard />
+      <BrowserRouter basename='/'>
+        <Route exact path="/" component={Dashboard} />
+        <Route exact path="/console" component={Console} />
+        <Route exact path="/tasks" component={Tasks} />
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
-
-
-/* 
-<BrowserRouter basename='/'>
-        <Route exact path="/" component={Dashboard} />
-        <Route exact path="/console" component={Console} />
-        <Route exact path="/tasks" component={Tasks} />
-      </BrowserRouter>
-*/
