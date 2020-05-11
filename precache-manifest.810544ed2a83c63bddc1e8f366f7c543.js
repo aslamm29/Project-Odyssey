97aeaa3517465dd1f5b4ff7a2648ecd5edb49756
aslamm <aslamm.anas29@gmail.com>
2020-05-11 12:21:26 -0400

    Updates

--- precache-manifest.810544ed2a83c63bddc1e8f366f7c543.js
+++ precache-manifest.810544ed2a83c63bddc1e8f366f7c543.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "58b553c25bfa4fa7c1a531a7406501b0",
+    "revision": "4a5f8fbae5d10df593f9e40d06303348",
     "url": "/Project-Odyssey/index.html"
   },
   {
-    "revision": "bac915309bc219859af8",
+    "revision": "fffe088db6475ce76794",
     "url": "/Project-Odyssey/static/css/main.88b0fd2c.chunk.css"
   },
   {
-    "revision": "352b65f82b2eb6dc41e0",
-    "url": "/Project-Odyssey/static/js/2.dc09aafe.chunk.js"
+    "revision": "befa85e89b8f3fcdc753",
+    "url": "/Project-Odyssey/static/js/2.2bde622e.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/Project-Odyssey/static/js/2.dc09aafe.chunk.js.LICENSE.txt"
+    "url": "/Project-Odyssey/static/js/2.2bde622e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "bac915309bc219859af8",
-    "url": "/Project-Odyssey/static/js/main.eea1c0c2.chunk.js"
+    "revision": "fffe088db6475ce76794",
+    "url": "/Project-Odyssey/static/js/main.2d025b57.chunk.js"
   },
   {
     "revision": "7d1b398d059f54e1c9eb",
